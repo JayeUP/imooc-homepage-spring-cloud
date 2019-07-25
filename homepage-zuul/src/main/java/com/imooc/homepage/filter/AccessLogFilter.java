b38ecf1f6e3f006df6cfe951b73dca7ce08435b0
金元 <820058477@qq.com>
2019-07-25 21:04:45 +0800

    3-4 zuul更新

---
+++ homepage-zuul/src/main/java/com/imooc/homepage/filter/AccessLogFilter.java
@@ -0,0 +1,27 @@
+package com.imooc.homepage.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+public class AccessLogFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
